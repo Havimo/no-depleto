3ee10da7cf3d8c31e7a13679ab286314f4afb23d
adecastro <adrienhavimo@gmail.com>
2024-03-23 20:39:08 +0100

    first frame

--- NoDepleto.lua
+++ NoDepleto.lua
@@ -18,6 +18,22 @@ local function PrintArray(tbl,indent)
     end
 end
 
+local function PrintArraytoString(tbl,indent,output_string)
+    if not indent then indent = 0 end
+    for k, v in pairs(tbl) do
+        formatting = string.rep("  ", indent) .. k .. ": "
+        if type(v) == "table" then
+            output_string = output_string .. formatting .. string.char(10)
+            output_string = PrintArraytoString(v, indent+1, output_string)
+        elseif type(v) == 'boolean' then
+            output_string = output_string .. formatting .. tostring(v) .. string.char(10) 
+        else
+            output_string = output_string .. formatting .. v .. string.char(10) 
+        end
+    end
+    return output_string
+end
+
 local function SortArray(array, col) 
 
     local function compare_col(col)
@@ -31,8 +47,17 @@ local function SortArray(array, col)
     PrintArray(array)
 end
 
+local UIConfig = CreateFrame("Frame", "NoDepleto_UIConfig", UIParent, "BasicFrameTemplatewithInset")
 
-local player_list = {}
+UIConfig:SetSize(500, 600)
+UIConfig:SetPoint("CENTER", UIParent, "CENTER")
+UIConfig.title = UIConfig:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
+UIConfig.title:SetPoint("LEFT", UIConfig.TitleBg, "LEFT", 5, 0)
+UIConfig.title:SetText("No Depleto - M+ List Filter")
+
+UIConfig.body = UIConfig:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
+UIConfig.body:SetPoint("CENTER", UIConfig, "CENTER")
+
 EventFrame:SetScript("OnEvent", function(self, event, ...)
     if(event == "LFG_LIST_APPLICANT_LIST_UPDATED" or event == "LFG_LIST_APPLICATION_STATUS_UPDATED" or event == "LFG_LIST_APPLICANT_UPDATED") then
 
@@ -50,5 +75,12 @@ EventFrame:SetScript("OnEvent", function(self, event, ...)
             table.insert(player_list, player)
         end 
         SortArray(player_list, 'dungeonScore')
+
+        UIConfig.body:SetText(PrintArraytoString(player_list, 0, ""))  
     end
 end)
+
+
+
+
+
